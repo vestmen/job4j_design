273c2c3c389739b6c8ba41431ee1197f77372307
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-03-12 11:09:06 +0300

    Утверждения с примитивными типами [#504883]

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -28,42 +28,42 @@ class BoxTest {
     }
 
     @Test
-    void WhenNumberOfVertex3ThenLessThan0() {
+    void whenNumberOfVertex3ThenLessThan0() {
         Box box = new Box(3, 10);
         int rsl = box.getNumberOfVertices();
         assertThat(rsl).isLessThan(0);
     }
 
     @Test
-    void WhenNumberOfVertex4ThenPositive() {
+    void whenNumberOfVertex4ThenPositive() {
         Box box = new Box(4, 10);
         int rsl = box.getNumberOfVertices();
         assertThat(rsl).isPositive();
     }
 
     @Test
-    void IsNotExist() {
+    void isNotExist() {
         Box box = new Box(3, 10);
         boolean rsl = box.isExist();
         assertThat(rsl).isFalse();
     }
 
     @Test
-    void IsExist() {
+    void isExist() {
         Box box = new Box(4, 10);
         boolean rsl = box.isExist();
         assertThat(rsl).isTrue();
     }
 
     @Test
-    void WhenVertex4Edge10Then() {
+    void whenVertex4Edge10Then() {
         Box box = new Box(4, 10);
         double rsl = box.getArea();
         assertThat(rsl).isEqualTo(173.21d, withPrecision(0.006d));
     }
 
     @Test
-    void WhenVertex3Edge10Then0() {
+    void whenVertex3Edge10Then0() {
         Box box = new Box(3, 10);
         double rsl = box.getArea();
         assertThat(rsl).isEqualTo(0);
