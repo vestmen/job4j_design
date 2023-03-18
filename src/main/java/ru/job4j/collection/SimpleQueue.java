c30c672e7d21c15ec361d383686984a6385723d2
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-03-18 18:17:31 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -9,7 +9,7 @@ public class SimpleQueue<T> {
     private int sizeOut = 0;
 
     public T poll() {
-        if (sizeIn == 0) {
+        if (sizeIn == 0 && sizeOut == 0) {
             throw new NoSuchElementException();
         }
         if (sizeOut == 0) {
