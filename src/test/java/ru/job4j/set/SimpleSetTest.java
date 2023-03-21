ae92ae0f83033c47de653e58fed5d7d001727bb4
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-03-21 19:02:41 +0300

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/test/java/ru/job4j/set/SimpleSetTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.set;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleSetTest {
+
+    @Test
+    void whenAddNonNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+    }
+
+    @Test
+    void whenAddNull() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(null)).isTrue();
+        assertThat(set.contains(null)).isTrue();
+        assertThat(set.add(null)).isFalse();
+    }
+
+    @Test
+    void whenAdd1231() {
+        Set<Integer> set = new SimpleSet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.add(2)).isTrue();
+        assertThat(set.add(3)).isTrue();
+        assertThat(set.add(1)).isFalse();
+        assertThat(set.contains(3)).isTrue();
+    }
+}
