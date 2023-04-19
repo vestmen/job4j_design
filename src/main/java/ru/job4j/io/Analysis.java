d43fb9a2b5589ad35a8ab4b165eb1ff30a1dcbd5
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-04-19 22:35:21 +0300

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    private boolean check = true;
+
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+                in.lines()
+                        .forEach(a -> {
+                            String[] words = a.split(" ");
+                            if ((check && (a.contains("400") || a.contains("500")))) {
+                                check = false;
+                                out.print(String.format(words[1] + " ; "));
+                            }
+                            if (!check && (a.contains("200") || a.contains("300"))) {
+                                check = true;
+                                out.print(String.format(words[1] + System.lineSeparator()));
+                            }
+                        });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
