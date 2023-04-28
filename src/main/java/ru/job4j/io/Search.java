881be81f895ccc372d91418ec9469393bcc404c2
AlexNemtsev <alex.nemtsev95@gmail.com>
2023-04-28 22:05:57 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,6 +10,7 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        validate(args);
         Path start = Paths.get(".");
         search(start, p -> p.toFile().getName().endsWith(".xml")).forEach(System.out::println);
     }
@@ -18,5 +19,18 @@ public class Search {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        File file = new File(args[0]);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("The launch requires 2 parameters");
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        if (!args[1].startsWith(".") || args[1].length() < 2) {
+            throw new IllegalArgumentException(String.format("Not an extension %s", args[1]));
+        }
     }
 }
